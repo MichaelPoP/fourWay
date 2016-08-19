4b63207ec0e68bce213ffc5fac53b0bc3aaf25c4
Michael <michaelcpospisil@gmail.com>
2016-08-19 05:21:49 -0700

    calling it here

--- routes/index.js
+++ routes/index.js
@@ -3,35 +3,35 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'FourWayStop' });
+  res.render('index', { title: 'FourWayStop', inst: 'Press ENTER to start the animation' });
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About Page' });
+  res.render('about', { title: 'About Page', inst: 'Press ENTER to start the animation' });
 });
 
 router.get('/twoeven', function(req, res, next) {
-  res.render('twoeven', { title: 'Two Even' });
+  res.render('twoeven', { title: 'Two Even', inst: 'Press ENTER to start the animation' });
 });
 
 router.get('/rightofway', function(req, res, next) {
-  res.render('rightofway', { title: 'Right of Way' });
+  res.render('rightofway', { title: 'Right of Way', inst: 'Press ENTER to start the animation' });
 });
 
 router.get('/straightfirst', function(req, res, next) {
-  res.render('straightfirst', { title: 'Straight First' });
+  res.render('straightfirst', { title: 'Straight First', inst: 'Press ENTER to start the animation' });
 });
 
 router.get('/pedestrian', function(req, res, next) {
-  res.render('pedestrian', { title: 'Pedestrian' });
+  res.render('pedestrian', { title: 'Pedestrian', inst: 'Press ENTER to start the animation' });
 });
 
 router.get('/emergency', function(req, res, next) {
-  res.render('emergency', { title: 'Emergency' });
+  res.render('emergency', { title: 'Emergency', inst: 'Press ENTER to start the animation' });
 });
 
 router.get('/emergency2', function(req, res, next) {
-  res.render('emergency2', { title: 'Emergency Motorcade' });
+  res.render('emergency2', { title: 'Emergency Motorcade', inst: 'Press ENTER to start the animation' });
 });
 
 module.exports = router;
